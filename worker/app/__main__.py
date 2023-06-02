09d6bb79fad6cea8adf0eb9282d8b4b7f0891ccc
justusmattern27 <justus.mattern@rwth-aachen.de>
2023-06-02 15:44:20 +0200

    inference worker
    Former-commit-id: f96aa97e9db481ebf45e37b4e1ad32751fb5c97e

--- worker/app/__main__.py
+++ worker/app/__main__.py
@@ -4,17 +4,26 @@ Worker entry point
 """
 
 from flask import Flask, request
+from inference_worker.haven.inference_server import InferenceClient
+import os
+
+ABSOLUTE_PATH = os.path.dirname(__file__)
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "HELLO!"
 
-@app.route("/echo", methods=["POST"])
+inference_client = InferenceClient(config=os.path.join(ABSOLUTE_PATH, "inference_worker/config/mpt_chat_7b.json"), setup_type="16bit")
+
+@app.route("/generate", methods=["POST"])
 def echo():
     data = request.get_json()
-    return data
+    prompt = data["prompt"]
+    config = data["config"]
+
+    output = inference_client.generate(text_input=prompt, **config)
+    
+    return output
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
