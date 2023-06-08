02053d36f3a6ecd1bb44d799aa0b01caf1f3f8a6
hkonsti <45428767+hkonsti@users.noreply.github.com>
2023-06-08 18:55:14 +0200

    update sdk proto script to new folder

--- sdk/proto.sh
+++ sdk/proto.sh
@@ -5,13 +5,13 @@
 # Generates proto files
 pip install grpcio-tools==1.51.1 mypy-protobuf==3.4.0 'types-protobuf>=3.20.4'
 
-rm -r ./haven/pb || true
-mkdir -p ./haven/pb
+rm -r ./havenllm/pb || true
+mkdir -p ./havenllm/pb
 
 python -m grpc_tools.protoc -I../proto \
-		--python_out=./haven/pb \
-		--mypy_out=./haven/pb \
-		--grpc_python_out=./haven/pb ../proto/manager.proto
+		--python_out=./havenllm/pb \
+		--mypy_out=./havenllm/pb \
+		--grpc_python_out=./havenllm/pb ../proto/manager.proto
 
-find haven/pb/ -type f -name "*.py" -print0 -exec sed -i -e 's/^\(import.*pb2\)/from . \1/g' {} \;
-touch haven/pb/__init__.py
+find havenllm/pb/ -type f -name "*.py" -print0 -exec sed -i -e 's/^\(import.*pb2\)/from . \1/g' {} \;
+touch havenllm/pb/__init__.py
