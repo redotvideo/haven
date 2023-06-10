5bb1888729675c5f1775dc21e4ecce6dda3ce90a
justusmattern27 <justus.mattern@rwth-aachen.de>
2023-06-11 00:18:57 +0200

    total worker rewrite
    Former-commit-id: 1da03b580fa0838c14ed2412993abd2d7d8c95ef

--- worker/app/inference_worker/test.py
+++
@@ -1,6 +0,0 @@
-from haven.inference_server import InferenceClient
-
-client = InferenceClient("config/mpt_chat_7b.json", setup_type="T4_8bit")
-output = client.generate("Hey, schlag mir bitte ein paar Date Ideen vor! Was koennte ich mit meiner Freundin machen?")
-
-print("output", output)
