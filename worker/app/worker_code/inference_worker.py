82b1ac86945e340008672b78ec3716ac2691aba0
justusmattern27 <justus.mattern@rwth-aachen.de>
2023-06-13 12:37:53 +0000

    some import changes

---
+++ worker/app/worker_code/inference_worker.py
@@ -0,0 +1,32 @@
+import transformers
+from transformers import StoppingCriteria, StoppingCriteriaList, TextIteratorStreamer
+import torch
+import json
+from threading import Thread
+from typing import List
+
+from model_registry import ModelRegistry
+from inference_utils.stopping_criteria import StopOnTokens
+
+
+
+class InferenceClient:
+    def __init__(self, config):
+
+        with open(config, 'r') as f:
+            self.config = json.load(f)
+
+        self.model_engine = ModelRegistry.REGISTRY[self.config["base_class"]](self.config["model_config"])
+        self.model_engine.prepare_for_inference(int8_quantization=self.config["int8"])
+        
+
+    def generate(self, text_input, **kwargs):
+        return self.model_engine.generate(text_input, **kwargs)
+
+        
+
+#if __name__ == '__main__':
+client = InferenceClient("models/model_configs/mpt_chat_7b_newconfig.json")
+stream = client.generate("Hey!! How are you doing?")
+for s in stream:
+    print(s)
