2e64fa4c83d21bc31f147b0dbbe541f7de2b53a3
justusmattern27 <justus.mattern@rwth-aachen.de>
2023-06-18 18:44:01 -0700

    fix
    Former-commit-id: a750b1c1c81838ad2f4fb1beb3825fc51e6bfe18

--- worker/app/worker/models/falcon_7b.py
+++ worker/app/worker/models/falcon_7b.py
@@ -19,5 +19,5 @@ class Falcon7BModel(AutoCausalModel):
         self.model = transformers.AutoModelForCausalLM.from_pretrained(self.model_config["model_name"], device_map="auto", load_in_8bit=int8_quantization, trust_remote_code=True, torch_dtype=torch.bfloat16)
         self.tokenizer = transformers.AutoTokenizer.from_pretrained(self.model_config["model_name"])
 
-    def generate_stream(self, text_input: str, sample: bool = True, top_p: float = 0.8, top_k: int = 500, temperature: float = 0.9, max_length: int = 2048):
-        return super().generate_stream(text_input, sample, top_p, top_k, temperature, max_length)
+    def generate_stream(self, text_input: str, conversation_history: List, sample: bool = True, top_p: float = 0.8, top_k: int = 500, temperature: float = 0.9, max_length: int = 2048):
+        return super().generate_stream(text_input, conversation_history, sample, top_p, top_k, temperature, max_length)
