94e3ccb99088346acd33a00c53fae26422d9b84b
Justus Mattern <122226645+justusmattern27@users.noreply.github.com>
2023-06-20 17:41:40 -0700

    Lora Finetuning for Falcon and Base Model (#11)
    Former-commit-id: c7b0b908cd1abdd203dfa66498aee2917bf29543

--- worker/app/worker/models/falcon_7b.py
+++ worker/app/worker/models/falcon_7b.py
@@ -3,10 +3,14 @@ from transformers import TextIteratorStreamer, StoppingCriteriaList
 from threading import Thread
 import torch
 import deepspeed
-from .inference_utils.stopping_criteria import StopOnTokens
+from peft import LoraConfig, prepare_model_for_int8_training, get_peft_model
 from typing import List
 
 from models.base_causal import AutoCausalModel
+from .training_utils.tokenizer_resize import resize_tokenizer_and_embeddings
+from .training_utils.data_processing import make_supervised_data_module
+from .inference_utils.stopping_criteria import StopOnTokens
+
 
 
 class Falcon7BModel(AutoCausalModel):
@@ -30,3 +34,29 @@ class Falcon7BModel(AutoCausalModel):
         
     def generate_stream(self, text_input: str, conversation_history: List, sample: bool = True, top_p: float = 0.8, top_k: int = 500, temperature: float = 0.9, max_length: int = 2048):
         return super().generate_stream(text_input, conversation_history, sample, top_p, top_k, temperature, max_length)
+
+
+    ##############################
+    ### INFERENCE    #############
+    ##############################
+    def prepare_model_for_training(self):
+        self.model = transformers.AutoModelForCausalLM.from_pretrained(self.model_config["model_name"], device_map="auto", load_in_8bit=self.model_config["lora"], trust_remote_code=True, torch_dtype=torch.bfloat16)
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(self.model_config["model_name"])
+
+        if self.tokenizer.pad_token is None:
+            resize_tokenizer_and_embeddings(
+                tokenizer=self.tokenizer,
+                model=self.model,
+            )
+
+        if self.model_config["lora"]:
+            lora_config = LoraConfig(r=16, lora_alpha=32, target_modules=self.model_config["lora_params"], lora_dropout=0.05, bias="none", task_type="CAUSAL_LM")
+            self.model = prepare_model_for_int8_training(self.model)
+            self.model = get_peft_model(self.model, lora_config)
+
+
+    def prepare_data_for_training(self):
+        super().prepare_data_for_training()
+
+    def train(self):
+        super().train()
