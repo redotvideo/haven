59cf53f4c5661d63ff3ae0f579fabe2def880a93
justusmattern27 <justus.mattern@rwth-aachen.de>
2023-07-01 22:12:16 -0700

    model implementations for vllm

---
+++ worker/app/worker/models/llama_13b.py
@@ -0,0 +1,26 @@
+from typing import List
+from .vllm_causal import VllmCausalModel
+
+
+class Llama13B(VllmCausalModel):
+
+    architecture_name = "llama_13b"
+
+    def __init__(self, config):
+        super().__init__(config)
+
+
+    ##############################
+    ### INFERENCE    #############
+    ##############################
+    def prepare_for_inference(self):
+        super().prepare_for_inference()
+
+
+    async def generate_stream(self, messages: List, top_p: float = 0.8, top_k: int = 500, temperature: float = 0.9, max_length: int = 8000):
+        super().generate_stream(messages, top_p, top_k, temperature, max_length)
+
+
+    def create_prompt_from_messages(self, messages):
+        super().create_prompt_from_messages(messages)
+
