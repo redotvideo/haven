c6aa2b6b10f6d3eeb05c8453d8876c973929cdd0
Konsti <45428767+hkonsti@users.noreply.github.com>
2023-07-11 16:54:31 -0700

    v0.2.0 (#69)

--- manager/lib/config.ts
+++ manager/lib/config.ts
@@ -1,5 +1,5 @@
 const config = {
-	version: "0.1.0", // Current version of the manager
+	version: "0.2.0", // Current version of the manager
 	setupDone: false, // Tells us if the manager is in a working state
 	server: {
 		bearerToken: process.env.BEARER_TOKEN || "insecure",
@@ -11,7 +11,7 @@ const config = {
 		clientId: "", // Set during setup
 	},
 	worker: {
-		dockerImage: process.env.CUSTOM_WORKER_IMAGE || "docker.io/havenhq/worker:v0.1",
+		dockerImage: process.env.CUSTOM_WORKER_IMAGE || "docker.io/havenhq/worker:v0.2",
 		startupScript: "./config/gcp/startup-script.sh",
 	},
 	telemetry: process.env.DISABLE_TELEMETRY !== "true",
